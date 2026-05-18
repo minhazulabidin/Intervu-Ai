57feab7a946937a950437423aeb2c6f111dccdb3
minhazulabidin <minhajulabedin648@gmail.com>
2026-05-18 13:33:40 +0600

    interview page a data fetch done

--- frontend/app/dashboard/interview/[interviewId]/page.jsx
+++ frontend/app/dashboard/interview/[interviewId]/page.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+"use client";
+
+import axios from "axios";
+import { useParams } from "next/navigation";
+import React, { useEffect, useState } from "react";
 
 const InterviewId = () => {
-  return (
-    <div>InterviewId</div>
-  )
-}
+  const [question, setQuestion] = useState([]);
 
-export default InterviewId
+  const params = useParams();
+  const id = params?.interviewId;
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          `${process.env.NEXT_PUBLIC_SERVER_URL}/questions/getQuestions/${id}`
+        );
+
+        setQuestion(response?.data?.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    if (id) {
+      fetchData();
+    }
+  }, [id]);
+
+  console.log(question);
+
+  return <div>InterviewId</div>;
+};
+
+export default InterviewId;
