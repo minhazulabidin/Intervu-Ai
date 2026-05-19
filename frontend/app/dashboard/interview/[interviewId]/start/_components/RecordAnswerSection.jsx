55058c87ad9ba8ca4abc658fb6f51beddd28b86a
minhazulabidin <minhajulabedin648@gmail.com>
2026-05-19 15:41:52 +0600

    handleSpeechToText fn done

--- frontend/app/dashboard/interview/[interviewId]/start/_components/RecordAnswerSection.jsx
+++ frontend/app/dashboard/interview/[interviewId]/start/_components/RecordAnswerSection.jsx
@@ -24,6 +24,15 @@ const RecordAnswerSection = () => {
     useEffect(() => {
         results && results.map(result => setUserAnswer(prev => prev + result.transcript))
     }, [results])
+
+    const handleSpeechToText = () => {
+        if (isRecording) {
+            stopSpeechToText()
+        } else {
+            startSpeechToText()
+        }
+    }
+
     return (
         <div className='flex flex-col justify-center items-center'>
             <div className=' rounded-lg bg-black my-10 flex flex-col justify-center items-center w-full'>
@@ -40,7 +49,7 @@ const RecordAnswerSection = () => {
             <Button
                 variant='outline'
                 className="rounded-lg cursor-pointer"
-                onClick={isRecording ? stopSpeechToText : startSpeechToText}
+                onClick={handleSpeechToText}
             >
                 {
                     isRecording ? <h2 className='flex items-center gap-1 text-red-600'><Mic /> Recording</h2> : <h2 className='text-purple-600'>Start Recording</h2>
