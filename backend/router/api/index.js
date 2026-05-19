c09cd7b204c1b97e6a3d3865a91655b294828fd5
introtoxicXXl <96826439+minhazulabidin@users.noreply.github.com>
2026-05-19 22:48:45 +0600

    feedback route create

--- backend/router/api/index.js
+++ backend/router/api/index.js
@@ -4,5 +4,6 @@ const router = express.Router();
 router.use("/questions", require("./questions"));
 router.use("/user", require("./user"));
 router.use("/webhooks", require("./webhooks"));
+router.use("/feedback", require("./feedback"));
 
 module.exports = router;
