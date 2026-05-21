395141da1f8ceb2679d63c78b90d2f13e89c2fe5
introtoxicXXl <96826439+minhazulabidin@users.noreply.github.com>
2026-05-21 15:33:36 +0600

    feedback populate done

--- frontend/app/dashboard/interview/[interviewId]/feedback/page.jsx
+++ frontend/app/dashboard/interview/[interviewId]/feedback/page.jsx
@@ -1,14 +1,31 @@
-import React from 'react'
+"use client";
+import React, { useState,useEffect } from 'react'
 import { Button } from '@/components/ui/button';
-// import { ChevronDownIcon } from '@radix-ui/react-icons';
 import {
     Collapsible,
     CollapsibleContent,
     CollapsibleTrigger,
 } from "@/components/ui/collapsible"
 import { ChevronDownIcon } from 'lucide-react';
+import { Card, CardContent } from '@/components/ui/card';
+import { useParams } from 'next/navigation';
+import axios from 'axios';
 
 const FeedbackPage = () => {
+    const [feedBacks,setFeedbacks] = useState([]);
+    const params = useParams();
+    const id = params?.interviewId;
+console.log(feedBacks)
+    useEffect(()=>{
+        const fetchData =async()=>{
+            const res = await axios.get(`${process.env.NEXT_PUBLIC_SERVER_URL}/questions/getQuestions/${id}`);
+            setFeedbacks(res?.data?.data?.feedback)
+        }
+        if(!feedBacks.length){
+            fetchData()
+        }
+    },[])
+
     return (
         <main>
             <section className="w-full max-w-7xl mx-auto">
@@ -18,23 +35,27 @@ const FeedbackPage = () => {
                     <h4 className="text-blue-800 lg:text-xl md:text-lg text-md">Your overall interview rating is 7/10</h4>
                     <p className='text-sm'>Find below interview question with correct answer, Your answer and feedback for improvement</p>
                 </div>
+
                 <div>
-                    
-                    <Collapsible className="rounded-md data-open:bg-muted">
-                        <CollapsibleTrigger render={<Button variant="ghost" className="w-full">Product details
-                            <ChevronDownIcon className="ml-auto group-data-panel-open/button:rotate-180" /></Button>} />
-                        <CollapsibleContent className="flex flex-col items-start gap-2 p-2.5 pt-0 text-sm">
-                            <div>
-                                This panel can be expanded or collapsed to reveal additional
-                                content.
-                            </div>
-                            <Button size="xs">Learn More</Button>
-                        </CollapsibleContent>
-                    </Collapsible>
+                    <Card className="mx-auto w-full">
+                        <CardContent>
+                            <Collapsible className="rounded-md data-open:bg-muted">
+                                <CollapsibleTrigger render={<Button variant="ghost" className="w-full">Product details
+                                    <ChevronDownIcon className="ml-auto group-data-panel-open/button:rotate-180" /></Button>} />
+                                <CollapsibleContent className="flex flex-col items-start gap-2 p-2.5 pt-0 text-sm">
+                                    <div>
+                                        This panel can be expanded or collapsed to reveal additional
+                                        content.
+                                    </div>
+                                    <Button size="xs">Learn More</Button>
+                                </CollapsibleContent>
+                            </Collapsible>
+                        </CardContent>
+                    </Card>
                 </div>
             </section>
         </main>
     )
 }
 
-export default FeedbackPage
+export default FeedbackPage;
