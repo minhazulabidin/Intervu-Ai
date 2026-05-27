a8a7ce6fd7177973cbf3b8f7fa1914e34ae0e8c4
minhazulabidin <minhajulabedin648@gmail.com>
2026-05-27 17:16:12 +0600

    update

--- frontend/app/layout.js
+++ frontend/app/layout.js
@@ -106,6 +106,7 @@ export default function RootLayout({ children }) {
               <Toaster />
 
               {children}
+              <Analytics />
             </LayoutGroup>
           </IntroProvider>
         </body>
